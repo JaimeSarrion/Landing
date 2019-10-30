733b3ed9e31956b9f789c7bd4ffacf5f6d113e02
Jaime Sarrion <jss72@alu.ua.es>
2019-10-30 16:10:42 +0100

    Redes sociales

--- landing/src/components/Redes/Redes.js
+++ landing/src/components/Redes/Redes.js
@@ -13,11 +13,11 @@ class Redes extends Component {
                 <h3 className={classes.Titulo}>
                     Redes Sociales
                 </h3>
-                <a href='/' className={classes.Circulo}>
+                <a href='https://github.com/JaimeSarrion' className={classes.Circulo}>
                     <GitHubIcon fontSize="large" htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white"/>
                 </a>
-                <a href='/' className={classes.Circulo}>
-                    <LinkedInIcon fontSize="50px" htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white"/>
+                <a href='https://www.linkedin.com/in/jaime-sarrion-sahuquillo/' className={classes.Circulo}>
+                    <LinkedInIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white"/>
                 </a>
             </div>
         )
