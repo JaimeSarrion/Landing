c64ac647f61b7715140a4a4dd1bfed04aa3cc023
Jaime Sarrión <jaime.sarrion@vipera.com>
2020-03-23 09:30:01 +0100

    Fixed an style error

--- landing/src/components/Competencias/Competencia/Competencia.js
+++ landing/src/components/Competencias/Competencia/Competencia.js
@@ -1,49 +1,78 @@
-import React, { Component } from 'react'
-import classes from './Competencia.module.css'
+import React, { Component } from 'react';
+import classes from './Competencia.module.css';
 import AndroidIcon from '@material-ui/icons/Android';
 import BrushIcon from '@material-ui/icons/Brush';
 import DesktopMacIcon from '@material-ui/icons/DesktopMac';
 import SettingsRemoteIcon from '@material-ui/icons/SettingsRemote';
 
 class Competencia extends Component {
-
-
-    render() {
-        let icon = "";
-        switch (this.props.Icono) {
-            case "Android":
-                icon = (<AndroidIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white" ></AndroidIcon>)
-                break;
-            case "Desktop":
-                icon = (<DesktopMacIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white" ></DesktopMacIcon>)
-                break;
-            case "Brush":
-                icon = (<BrushIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white" ></BrushIcon>)
-                break;
-            case "IoT":
-                icon = (<SettingsRemoteIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white" ></SettingsRemoteIcon>)
-                break;
-            default:
-                icon = (<DesktopMacIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white" ></DesktopMacIcon>)
-                break;
-
-        }
-
-        return (
-            <div className={classes.Container}>
-                <div className={classes.Circulo}>
-                    {icon}
-                </div>
-                <div>
-                    <h2>
-                        {this.props.Nombre}
-                    </h2>
-                    <p>{this.props.Desc}</p>
-                </div>
-            </div>
-
-        )
+  render() {
+    let icon = '';
+    switch (this.props.Icono) {
+      case 'Android':
+        icon = (
+          <AndroidIcon
+            htmlColor='whitesmoke'
+            fontSize='large'
+            style={{ marginTop: '36px' }}
+            color='white'
+          ></AndroidIcon>
+        );
+        break;
+      case 'Desktop':
+        icon = (
+          <DesktopMacIcon
+            htmlColor='whitesmoke'
+            fontSize='large'
+            style={{ marginTop: '36px' }}
+            color='white'
+          ></DesktopMacIcon>
+        );
+        break;
+      case 'Brush':
+        icon = (
+          <BrushIcon
+            htmlColor='whitesmoke'
+            fontSize='large'
+            style={{ marginTop: '36px' }}
+            color='white'
+          ></BrushIcon>
+        );
+        break;
+      case 'IoT':
+        icon = (
+          <SettingsRemoteIcon
+            htmlColor='whitesmoke'
+            fontSize='large'
+            style={{ marginTop: '36px' }}
+            color='white'
+          ></SettingsRemoteIcon>
+        );
+        break;
+      default:
+        icon = (
+          <DesktopMacIcon
+            htmlColor='whitesmoke'
+            fontSize='large'
+            style={{ marginTop: '36px' }}
+            color='white'
+          ></DesktopMacIcon>
+        );
+        break;
     }
+
+    return (
+      <div className={classes.Container}>
+        <div className={classes.ContCirculo}>
+          <div className={classes.Circulo}>{icon}</div>
+        </div>
+        <div>
+          <h2>{this.props.Nombre}</h2>
+          <p>{this.props.Desc}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Competencia
+export default Competencia;
