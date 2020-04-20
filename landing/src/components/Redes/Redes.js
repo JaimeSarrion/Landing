d884bfd6b0abf79de05fe02df47f1f05d997f1c2
Jaime Sarrión <jaime.sarrion@vipera.com>
2020-04-20 11:51:01 +0200

    Añadido correo de contacto

--- landing/src/components/Redes/Redes.js
+++ landing/src/components/Redes/Redes.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import classes from './Redes.module.css'
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailIcon from '@material-ui/icons/Mail';
+
 
 
 class Redes extends Component {
@@ -18,6 +20,9 @@ class Redes extends Component {
                 </a>
                 <a href='https://www.linkedin.com/in/jaime-sarrion-sahuquillo/' className={classes.Circulo}>
                     <LinkedInIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white"/>
+                </a>
+                <a href = "mailto: jaime.sarrion.work@gmail.com" className={classes.Circulo}>
+                    <MailIcon htmlColor="whitesmoke" fontSize="large" style={{ marginTop: "36px" }} color="white"/>
                 </a>
             </div>
         )
